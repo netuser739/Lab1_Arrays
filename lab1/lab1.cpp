d6c200c50383a2f9055ed1dc04c587900fbfcb2d
Клим Коноплёв <melisakon45@gmail.com>
2023-10-22 17:37:06 +0300

    feat: функции для значений с плавающей точкой и выведения значений массивов после заполнения

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -33,7 +33,48 @@ void LowToUpArray(int* array, int size, int minValue, int maxValue) {
     }
 }
 
+void LowToUpArray(double* array, int size, int minValue, int maxValue) {
+    if (size >= (maxValue - minValue + 1)) {
+        for (int i = 0; i < size; i++) {
+            array[i] = minValue;
+            if (minValue < maxValue)
+                minValue++;
+        }
+    }
+    else {
+        int left = minValue;
+        int right = maxValue - size + 1;
+        for (int i = 0; i < size; i++) {
+            array[i] = (double)rand() / RAND_MAX * (right - left) + left;
+            left = array[i] + 1;
+            right++;
+        }
+    }
+}
+
 void UpToLowArray(int* array, int size, int minValue, int maxValue) {
+    if (size >= (maxValue - minValue + 1)) {
+        for (int i = 0; i < size; i++) {
+            array[i] = maxValue;
+            if (minValue < maxValue)
+                maxValue--;
+        }
+    }
+    else {
+        int left = maxValue - size + 1;
+        int right = maxValue;
+        for (int i = 0; i < size; i++) {
+            array[i] = (double)rand() / RAND_MAX * (right - left) + left;
+            right = array[i] - 1;
+            if (left > size)
+                left = right - size + 1;
+            else
+                left--;
+        }
+    }
+}
+
+void UpToLowArray(double* array, int size, int minValue, int maxValue) {
     if (size >= (maxValue - minValue + 1)) {
         for (int i = 0; i < size; i++) {
             array[i] = maxValue;
@@ -61,6 +102,12 @@ void RandomArray(int* array, int size, int minValue, int maxValue) {
     }
 }
 
+void RandomArray(double* array, int size, int minValue, int maxValue) {
+    for (int i = 0; i < size; i++) {
+        array[i] = (double)rand() / RAND_MAX * (maxValue - minValue) + minValue;
+    }
+}
+
 void SawArray(int* array, int size, int minValue, int maxValue, int step) {
     int dif = (maxValue - minValue) / step;
     array[0] = minValue;
@@ -74,7 +121,28 @@ void SawArray(int* array, int size, int minValue, int maxValue, int step) {
     }
 }
 
+void SawArray(double* array, int size, int minValue, int maxValue, int step) {
+    double dif = (maxValue - minValue) / step;
+    array[0] = minValue;
+    for (int i = 1; i < size; i++) {
+        if (array[i - 1] == maxValue || array[i - 1] > maxValue) {
+            array[i] = minValue;
+        }
+        else {
+            array[i] = array[i - 1] + dif;
+        }
+    }
+}
+
 void SinArray(int* array, int size, int minValue, int maxValue, int step) {
+    int ymove = (minValue + maxValue) / 2;
+    int arc = maxValue - ymove;
+    for (int i = 0; i < size; i++) {
+        array[i] = sin(i) * arc + ymove;
+    }
+}
+
+void SinArray(double* array, int size, int minValue, int maxValue, int step) {
     int ymove = (minValue + maxValue) / 2;
     int arc = maxValue - ymove;
     for (int i = 0; i < size; i++) {
@@ -99,6 +167,22 @@ void StepArray(int* array, int size, int minValue, int maxValue, int step) {
     }
 }
 
+void StepArray(double* array, int size, int minValue, int maxValue, int step) {
+    double h;
+    h = size / step;
+
+    float v = static_cast<float>(maxValue - minValue) / h;
+    float left = minValue;
+    float right;
+    for (int j = 0; j < h; j++) {
+        right = minValue + v * (j + 1);
+        for (int i = step * j; i < step * (j + 1); i++) {
+            array[i] = (double)rand() / RAND_MAX * (right - left) + left;
+        }
+        left = right;
+    }
+}
+
 void FunctionCall(void(*funcArr[])(int*, int, int, int), void(*funcStepArr[])(int*, int, int, int, int), int size) {
     
     int* arr = new int[size];
@@ -107,19 +191,19 @@ void FunctionCall(void(*funcArr[])(int*, int, int, int), void(*funcStepArr[])(in
     
     srand(time(NULL) + GetTickCount());
     
-    ofstream fin;
-    fin.open("test.txt");
+    ofstream ftime;
+    ftime.open("time.txt");
 
-    fin << "LowToUp\tUpToLow\tRandom\tSaw\tStep\tSin" << endl;
+    ftime << "LowToUp\tUpToLow\tRandom\tSaw\tStep\tSin" << endl;
 
-    if (fin.is_open()) {
+    if (ftime.is_open()) {
         for (int j = 0; j < 100; j++) {
             for (int i = 0; i < 3; i++) {
                 begin = chrono::steady_clock::now();
                 funcArr[i](arr, size, 0, 100);
                 end = chrono::steady_clock::now();
                 timeDiff = chrono::duration_cast<chrono::microseconds>(end - begin);
-                fin << timeDiff.count() << '\t';
+                ftime << timeDiff.count() << '\t';
             }
 
             for (int i = 0; i < 3; i++) {
@@ -127,14 +211,78 @@ void FunctionCall(void(*funcArr[])(int*, int, int, int), void(*funcStepArr[])(in
                 funcStepArr[i](arr, size, 0, 100, 8);
                 end = chrono::steady_clock::now();
                 timeDiff = chrono::duration_cast<chrono::microseconds>(end - begin);
-                fin << timeDiff.count() << '\t';
+                ftime << timeDiff.count() << '\t';
             }
 
-            fin << '\n';
+            ftime << '\n';
         }
     }
 
-    fin.close();
+    ftime.close();
+}
+
+void ArrayCreate(void(*funcArr[])(int*, int, int, int), void(*funcStepArr[])(int*, int, int, int, int), int size) {
+    int* arr = new int[size];
+    
+    srand(time(NULL) + GetTickCount());
+
+    ofstream fvalue;
+    fvalue.open("funcValue.txt");
+
+
+    if (fvalue.is_open()) {
+        
+        for (int i = 0; i < 3; i++) {
+            funcArr[i](arr, size, 0, 100);
+            for (int i = 0; i < size; i++) {
+                fvalue << arr[i] << '\t';
+            }
+            fvalue << '\n';
+
+        }
+
+        for (int i = 0; i < 3; i++) {
+            funcStepArr[i](arr, size, 40, 60, 30);
+            for (int i = 0; i < size; i++) {
+                fvalue << arr[i] << '\t';
+            }
+            fvalue << '\n';
+        }
+    }
+
+    fvalue.close();
+}
+
+void ArrayCreate(void(*funcArr[])(double*, int, int, int), void(*funcStepArr[])(double*, int, int, int, int), int size) {
+    double* arr = new double[size];
+
+    srand(time(NULL) + GetTickCount());
+
+    ofstream fvaluefloat;
+    fvaluefloat.open("funcValueFloat.txt");
+
+
+    if (fvaluefloat.is_open()) {
+
+        for (int i = 0; i < 3; i++) {
+            funcArr[i](arr, size, 0, 100);
+            for (int i = 0; i < size; i++) {
+                fvaluefloat << arr[i] << '\t';
+            }
+            fvaluefloat << '\n';
+
+        }
+
+        for (int i = 0; i < 3; i++) {
+            funcStepArr[i](arr, size, 30, 90, 30);
+            for (int i = 0; i < size; i++) {
+                fvaluefloat << arr[i] << '\t';
+            }
+            fvaluefloat << '\n';
+        }
+    }
+
+    fvaluefloat.close();
 }
 
 int main()
@@ -144,18 +292,12 @@ int main()
     void(*funcArr[])(int*, int, int, int) = {LowToUpArray, UpToLowArray, RandomArray};
     void(*funcStepArr[])(int*, int, int, int, int) = {SawArray, StepArray, SinArray };
 
-    //LowToUpArray(arr, size, 1, 50, 0);
-    //UpToLowArray(arr, size, 1, 99, 0);
-    //RandomArray(arr, size, 1, 99, 0);
-    //SawArray(arr, size, 10, 100, 10);
-    //StepArray(arr, size, 0, 200, 25);
-    //SinArray(arr, size, 10, 40, 4);
+    void(*funcArrFloat[])(double*, int, int, int) = { LowToUpArray, UpToLowArray, RandomArray };
+    void(*funcStepArrFloat[])(double*, int, int, int, int) = { SawArray, StepArray, SinArray };
 
-    /*for (int i = 0; i < size; i++) {
-        cout << arr[i] << ' ';
-    }*/
-
+    ArrayCreate(funcArr, funcStepArr, size);
     FunctionCall(funcArr, funcStepArr, size);
+    ArrayCreate(funcArrFloat, funcStepArrFloat, size);
 
 #pragma region Graphic
     /*RenderWindow window(VideoMode(W, H), "Graphic");
